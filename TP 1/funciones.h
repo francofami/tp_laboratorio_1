6087e790dc9cd841270a9679c01f49caa6dce0ab
francofami <31368175+francofami@users.noreply.github.com>
2018-05-05 22:53:41 -0300

    Rename funciones.h to TP 1/funciones.h

---
+++ TP 1/funciones.h
@@ -0,0 +1,40 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+/** \brief Calcula la suma
+ *
+ * \param numero1 float El número A (sumando).
+ * \param numero2 float El número B (sumando).
+ * \return float La suma entre el número A y el B.
+ *
+ */
+float sumar(float numero1, float numero2);
+
+/** \brief Calcula la resta.
+ *
+ * \param numero1 float El número A (minuendo).
+ * \param numero2 float El número B (sustraendo).
+ * \return float La diferencia entre número A y número B.
+ *
+ */
+float restar(float numero1, float numero2);
+
+/** \brief Calcula la division
+ *
+ * \param numero1 El numero A (dividendo).
+ * \param numero2 float El número B (divisor).
+ * \return float El cociente obtenido de la división entre el número A y el B.
+ *
+ */
+float dividir(float numero1, float numero2);
+
+/** \brief Calcula la multiplicacion
+ *
+ * \param numero1 float El número A (multiplicando).
+ * \param numero2 float El número B (multiplicador).
+ * \return float El producto entre el número A y el B.
+ *
+ */
+float multiplicar(float numero1, float numero2);
+
+#endif // FUNCIONES_H_INCLUDED
